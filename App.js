a321a1e0e5042dfc9eb9eba1ff5e4a540d470852
Astiom13 <Andric199688@gmail.com>
2022-02-06 13:47:49 +0200

    2

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello in Cash Controll app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    backgroundColor: '#f0f8ff'
+  }
 });
